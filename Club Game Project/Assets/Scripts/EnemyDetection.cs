0edcd885dc8859a45b3a6377108a7fdc4fb60626
mike54pfeiffer <32201579+mike54pfeiffer@users.noreply.github.com>
2020-08-16 23:14:21 -0700

    Added enemy Sprite+ Enemy movement/Detection scripts

---
+++ Club Game Project/Assets/Scripts/EnemyDetection.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    //Method will run once the player is within enemy sights
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //For future, add either an alert system, or just restart to last checkpoint
+        Debug.Log("Player Detected");
+    }
+}
---
+++ Club Game Project/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    //Variable Declaration
+    private Vector3 initialSpawn;
+    private bool moveUp;
+    public Rigidbody2D rb;
+
+    //Serialize Fields
+    [SerializeField] private float _speed = 250f;
+
+    private void Awake()
+    {
+        //Varibale Initialization
+        initialSpawn = this.transform.position;
+        moveUp = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        //Set rigidbody values and options
+        rb.gravityScale = 0;
+        rb.freezeRotation = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //Run if enemy has moved 5 units up from start
+        if(this.transform.position.y - initialSpawn.y > 5f)
+        {
+            //Set movement downward
+            moveUp = false;
+        }
+        
+        //Run if enemy has moved 5 units down from start
+        else if(this.transform.position.y - initialSpawn.y < -5f)
+        {
+            //set movement upward
+            moveUp = true;
+        }
+        
+        //Set the velocity of the enemy to be either upward or downward with the set speed
+        //NOTE: the Bool to Int conversion will act as multiplication of a 0 or a 1, so only one term will apply(Branchless Programming)
+        rb.velocity = _speed * Time.deltaTime * (Vector3.up * Convert.ToInt32(moveUp) + Vector3.down * Convert.ToInt32(!moveUp));
+    }
+}
