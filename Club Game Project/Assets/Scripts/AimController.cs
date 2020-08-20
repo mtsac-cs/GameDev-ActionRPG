96671c210c6adab8a1b4661b6f12020ab0f066ad
Mattheon <68176359+Mattheon@users.noreply.github.com>
2020-08-19 21:52:32 -0700

    Created Camera Follow Script
    -Created an empty game object named Global State Manager to handle things like menus and pause states (Should be replaced by something more intutive in the future)
    -Attached Reticle to the Player game object (Purely for organization. This does not effect the script or the reticle)
    -Created a Camera Follow script and detached the camera from the Player game object
    -Updated some obscure and incorrect comments in various scripts

--- Club Game Project/Assets/Scripts/AimController.cs
+++ Club Game Project/Assets/Scripts/AimController.cs
@@ -14,7 +14,7 @@ public class AimController : MonoBehaviour
     }
     void Update()
     {
-        // Grabs all the axis positions of the mouse, subtracts the z position by itself to get 0, and adds 1 to the z position in order to show the reticle infront of the screen
+        // Grabs all the axis positions of the mouse, gets the absolute value of the z axis, and adds 1 to the z axis in order to show the reticle infront of the camera
         mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(Input.mousePosition.z + 1));
 
         // Transforms the attached game object from screen space to world space, but because it uses the cameras z position which is at -1, the z axis of the reticle is at -1, therefore it will not 
---
+++ Club Game Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Camera mainCamera;
+    private GameObject player;
+    private Vector3 cameraPosition;
+
+    private void Awake()
+    {
+        // Reference the main camera
+        mainCamera = Camera.main;
+
+        // Reference to the player game object. If you change the player game objects name, change this string to that new name in order to allow this script to reference the player
+        player = GameObject.Find("Player");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Grabs all the axis positions of the player and subtracts 1 from the player's z axis
+        cameraPosition = new Vector3(player.transform.position.x, player.transform.position.y, (player.transform.position.z - 1));
+
+        // Assigns the above player position to the camera, except it subtracts 1 from the players position to always ensure the camera is in front of the player
+        mainCamera.transform.position = cameraPosition;
+    }
+}
