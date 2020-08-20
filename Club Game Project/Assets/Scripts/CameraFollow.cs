7aaf86c403347dfdd1c306b28157f1264d0aa2aa
Mattheon <68176359+Mattheon@users.noreply.github.com>
2020-08-20 16:39:09 -0700

    Added Aim Ahead Function
    -Added aim ahead function to the camera follow script

--- Club Game Project/Assets/Scripts/CameraFollow.cs
+++ Club Game Project/Assets/Scripts/CameraFollow.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Camera mainCamera;
     private GameObject player;
-    private Vector3 cameraPosition;
+    private GameObject reticle;
+    private Vector3 reticlePos;
+    private Vector3 playerPos;
+    private Vector3 relativePos;
 
     private void Awake()
     {
-        // Reference the main camera
-        mainCamera = Camera.main;
-
-        // Reference to the player game object. If you change the player game objects name, change this string to that new name in order to allow this script to reference the player
         player = GameObject.Find("Player");
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        // Grabs all the axis positions of the player and subtracts 1 from the player's z axis
-        cameraPosition = new Vector3(player.transform.position.x, player.transform.position.y, (player.transform.position.z - 1));
 
-        // Assigns the above player position to the camera, except it subtracts 1 from the players position to always ensure the camera is in front of the player
-        mainCamera.transform.position = cameraPosition;
+        reticle = GameObject.Find("Reticle");
+    }
+
+    private void Update()
+    {
+        playerPos = new Vector3(player.transform.position.x, player.transform.position.y, (player.transform.position.z - 1));
+
+        reticlePos = new Vector2(reticle.transform.position.x, reticle.transform.position.y);
+
+        Camera.main.WorldToScreenPoint(playerPos);
+        Camera.main.WorldToScreenPoint(reticlePos);
+
+        relativePos = (reticlePos - playerPos) / 3;
+        Debug.DrawRay(playerPos, relativePos);
+
+        transform.position = relativePos + playerPos;
+        Debug.Log(relativePos + playerPos);
     }
 }
